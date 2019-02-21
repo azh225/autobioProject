0add7ded4fe941677f486f51ce6e46327007ea2d
Ajali Harrison <azh225@nyu.edu>
2019-02-21 11:25:53 -0500

    first commit

---
+++ autobioProject/Assets/Scripts/fpsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsController : MonoBehaviour
+{
+    public Rigidbody thisRigidBody; //variable for the rigidbody
+    public Camera thisCamera;
+
+    public float fpForwardBackward;
+    public float fpStrafe;
+
+    public float pitch; //select a variable, press f2, can change the name of your variable everywhere in the script
+    public float yaw; // yaw is mouseX, pitch is mouseY
+
+    public Vector3 inputVelocity;
+    public float velocityModifier;
+
+    void Start()
+    {
+        thisRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        yaw = Input.GetAxis("Mouse X");
+        transform.Rotate(0, yaw, 0);
+
+        pitch = Input.GetAxis("Mouse Y");
+        thisCamera.transform.Rotate(-pitch, 0, 0);
+
+        fpForwardBackward = Input.GetAxis("Vertical");
+        fpStrafe = Input.GetAxis("Horizontal");
+
+        inputVelocity = transform.forward * fpForwardBackward;
+        inputVelocity += transform.right * fpStrafe;
+    }
+
+    void FixedUpdate()
+    {
+        thisRigidBody.velocity = inputVelocity * velocityModifier + (Physics.gravity * .69f);
+    }
+}
